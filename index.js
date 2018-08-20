6f62b8f341a3ad416eb25382f41f695d1da00391
Justin Chang <justinxchang@hotmail.com>
2018-08-20 23:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- let obj = JSON.parse(item);
- cart.push(`itemName: ${obj.itemName}, itemPrice: ${obj.itemPrice}`)
- return cart
+// let obj = JSON.parse(item);
+// cart.push(`itemName: ${obj.itemName}, itemPrice: ${obj.itemPrice}`)
+// return cart
 }
 
 function viewCart() {
