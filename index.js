f20521891c9857a2adfdd538d4afdb5daed6c19a
Justin Chang <justinxchang@hotmail.com>
2018-08-21 05:43:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for(let i = 0; i<cart.length; i++){
-    if(cart[i].item == item){
+    if(cart[i].itemName == item){
       cart.splice(i, 1)
       return cart
     } 
