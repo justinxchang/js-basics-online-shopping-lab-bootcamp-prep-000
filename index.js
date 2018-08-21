fcb7d36110d521b5066e0543409222639803e82d
Justin Chang <justinxchang@hotmail.com>
2018-08-21 03:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
     for(let i = 0; i<cart.length - 1; i++){
       newArray.push(` ${cart[i].itemName} at \$${cart[i].itemPrice}`);
     }
-    newArray.push(` and ${cart[cart.length].itemName} at \$${cart[cart.length].itemPrice}`)
+    newArray.push(` and ${cart[cart.length - 1].itemName} at \$${cart[cart.length - 1].itemPrice}`)
   }
   return `In your cart, you have${newArray}.`;
 }
