c4ecabd1ba2bef88e9bf34bb20ea14eb7f1dec04
Justin Chang <justinxchang@hotmail.com>
2018-08-21 03:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,10 +34,12 @@ function viewCart() {
 
 function total() {
   // write your code here
-  var sum = 0
+  var sum = 0;
+  var prices = []
   for(let i = 0; i<cart.length; i++){
-    
+    prices.push(cart[i].itemPrice);
   }
+  
 }
 
 function removeFromCart(item) {
