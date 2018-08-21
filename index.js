d5017962e546e18f0642b0e0f7ed6ac7f456837d
Justin Chang <justinxchang@hotmail.com>
2018-08-21 05:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(!cardNumber){
+  if(cardNumber === null){
     return "Sorry, we don't have a credit card on file for you."
   }
 }
