4ec629b1a60e3ada72a5d30dd60480aea70dfebe
Justin Chang <justinxchang@hotmail.com>
2018-08-21 00:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function viewCart() {
   if(cart.length === 0){
     return "Your shopping cart is empty."
   } else {
-  for(let i = 0; i<cart.length; i++){
-    newArray.push(` ${cart[i].itemName} at \$${cart[i].itemPrice}.`);
+  for(let i = 0; i<cart.length-1; i++){
+    newArray.push(` ${cart[i].itemName} at \$${cart[i].itemPrice}, and ${cart[i.length].itemName} at \$${cart[i.length].itemPrice}.`);
   }
   return `In your cart, you have${newArray}`
   }
